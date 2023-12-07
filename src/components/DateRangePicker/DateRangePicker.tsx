a15bce51c83110d3a5c645422c33f7c5357f41d9
LuukvE <l.vanegeraat@gmail.com>
2023-12-07 16:30:29 +0400

    Fix: Date range picker use cube date translation

--- src/components/DateRangePicker/DateRangePicker.tsx
+++ src/components/DateRangePicker/DateRangePicker.tsx
@@ -1,21 +1,9 @@
-import {
-  format,
-  subDays,
-  subYears,
-  endOfDay,
-  endOfYear,
-  endOfWeek,
-  endOfToday,
-  startOfDay,
-  endOfMonth,
-  startOfWeek,
-  startOfYear,
-  startOfMonth,
-  startOfToday
-} from 'date-fns';
+import { format } from 'date-fns';
+import * as chrono from 'chrono-node';
 import 'react-day-picker/dist/style.css';
-import React, { useEffect, useRef, useState } from 'react';
-import { CaptionProps, DateRange, DayPicker, useNavigation } from 'react-day-picker';
+import React, { useEffect, useState } from 'react';
+import { CaptionProps, DayPicker, useNavigation } from 'react-day-picker';
+import { dateParser } from "@cubejs-backend/api-gateway/dist/src/dateParser";
 
 import useFont from '../../hooks/useFont';
 
@@ -23,94 +11,67 @@ import '../index.css';
 import { CalendarIcon, ChevronDown, ChevronLeft, ChevronRight } from '../icons';
 
 const ranges = [
-  {
-    name: 'Custom'
-  },
-  {
-    name: 'Today',
-    from: startOfToday(),
-    to: endOfToday()
-  },
-  {
-    name: 'Yesterday',
-    from: startOfDay(subDays(new Date(), 1)),
-    to: endOfDay(subDays(new Date(), 1))
-  },
-  {
-    name: 'This week',
-    from: startOfWeek(new Date()),
-    to: endOfWeek(new Date())
-  },
-  {
-    name: 'Last 7 days',
-    from: startOfDay(subDays(new Date(), 7)),
-    to: endOfToday()
-  },
-  {
-    name: 'This month',
-    from: startOfMonth(new Date()),
-    to: endOfMonth(new Date())
-  },
-  {
-    name: 'Last 30 days',
-    from: startOfDay(subDays(new Date(), 30)),
-    to: endOfToday()
-  },
-  {
-    name: 'This year',
-    from: startOfYear(new Date()),
-    to: endOfYear(new Date())
-  },
-  {
-    name: 'Last year',
-    from: startOfYear(subYears(new Date(), 1)),
-    to: endOfYear(subYears(new Date(), 1))
-  }
+  '',
+  'Today',
+  'Yesterday',
+  'This week',
+  'Last 7 days',
+  'This month',
+  'Last 30 days',
+  'This quarter',
+  'This year',
+  'Last year'
 ];
 
-export default (props: { value?: DateRange; onChange: (v: DateRange) => void }) => {
-  const [option, setOption] = useState(0);
-  const onChange = useRef<Function>(props.onChange);
-  const [range, setRange] = useState<DateRange | undefined>(props.value);
+type TimeRange = {
+  to?: Date;
+  from?: Date;
+  relativeTimeString?: string;
+}
+
+export default (props: { value?: TimeRange; onChange: (v?: TimeRange) => void }) => {
+  const [range, setRange] = useState<TimeRange | undefined>(props.value);
 
   useFont();
 
   useEffect(() => {
-    onChange.current = props.onChange;
-  }, [props.onChange]);
+    if (!props.value?.relativeTimeString) return;
 
-  useEffect(() => {
-    if (range) onChange.current(range);
-  }, [range]);
+    const [from, to] = dateParser(props.value?.relativeTimeString, 'UTC');
 
-  useEffect(() => {
-    setRange(props.value);
+    if (!from || !to) return;
+
+    setRange({
+      ...props.value,
+      from: new Date(from),
+      to: new Date(to)
+    });
   }, [props.value]);
 
   return (
     <div className="flex justify-center">
       <div className="relative inline-flex border rounded-md border-[#d8dad9] h-11 w-[420px] text-[#101010] text-sm font-medium">
         <div className="border-r border-[#d8dad9] grow flex items-center p-4 max-w-[180px] hover:bg-[#f3f4f6] cursor-pointer group relative text-[14px]">
-          {ranges[option]?.name} <ChevronDown className="ml-auto" />
+          {range?.relativeTimeString || ((range?.from && range?.to) ? 'Custom' : 'Select')} <ChevronDown className="ml-auto" />
           <div className="hidden w-full group-hover:block absolute top-8 left-0 z-50 pt-4 pointer-events-auto opacity-100">
             <div className="rounded w-full border border-[#d8dad9] overflow-hidden">
-              {ranges.map((range, i) => (
-                <div
+              {ranges.map((relativeTimeString, i) => (
+                !!relativeTimeString && <div
                   onClick={() => {
-                    setOption(i);
+                    if (!ranges[i]) return;
 
-                    if (!ranges[i]?.from) return;
+                    const [from, to] = dateParser(relativeTimeString, 'UTC');
 
-                    setRange({ from: ranges[i]?.from, to: ranges[i]?.to });
+                    if (!from || !to) return;
+
+                    setRange({ relativeTimeString, from: new Date(from), to: new Date(to) });
                   }}
-                  className={`${
-                    i === 0 ? '' : 'border-t'
-                  } border-[#d8dad9] text-[14px] h-10 w-full flex items-center justify-center cursor-pointer hover:bg-[#f3f4f6] hover:text-[#333942] ${
-                    option === i ? 'bg-[#f3f4f6] text-[#333942]' : 'bg-white'
-                  }`}
+                  className={`${i === 0 ? '' : 'border-t'
+                    } border-[#d8dad9] text-[14px] h-10 w-full flex items-center justify-center cursor-pointer hover:bg-[#f3f4f6] hover:text-[#333942] ${(range?.relativeTimeString || '') === relativeTimeString ? 'bg-[#f3f4f6] text-[#333942]' : 'bg-white'
+                    }`}
                   key={i}
                 >
-                  {range.name}
+                  {relativeTimeString}
                 </div>
               ))}
             </div>
@@ -130,10 +91,11 @@ export default (props: { value?: DateRange; onChange: (v: DateRange) => void }) 
               }}
               weekStartsOn={1}
               mode="range"
-              selected={range}
+              selected={(range?.from && range?.to) ? { from: range.from, to: range.to } : undefined}
               onSelect={(range) => {
                 setRange(range);
-                setOption(0);
+
+                props.onChange(range?.from && range?.to ? { ...range } : undefined)
               }}
             />
           </div>
