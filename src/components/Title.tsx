ebc990c73f0e9a9c6144f793661a1c340ed94949
Tom Gardiner <tom@trevor.io>
2023-12-09 13:03:48 +0100

    set font to futura

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 
 export default ({ title }: { title?: string }) =>
   !!title && (
-    <h2 className="text-[#333942] text-[14px] font-bold justify-start flex mb-8">{title}</h2>
+    <h2 className="text-[#333942] text-[14px] font-bold font-futura justify-start flex mb-8">{title}</h2>
   );
