83c217f5456ef4327d8bc113806feeed44e2eac1
Tom Gardiner <tom@trevor.io>
2023-12-13 00:10:42 +0100

    convert js files to ts files

--- embeddable.config.js
+++ embeddable.config.js
@@ -3,8 +3,5 @@ import react from '@embeddable.com/sdk-react';
 
 export default defineConfig({
   plugins: [react],
-  pushBaseUrl: 'https://api.dev.embeddable.com',
-  audienceUrl: 'https://api.dev.embeddable.com/',
-  authDomain: 'embeddable-dev.eu.auth0.com',
-  authClientId: 'xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG'
+  // pushBaseUrl: 'https://api.dev.embeddable.com'
 });
