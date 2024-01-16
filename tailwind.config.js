36a1cc953ee3ecb177716982b0110765820b756e
Lucas van Egeraat <l.vanegeraat@gmail.com>
2024-01-16 15:05:52 +0400

    Chore: Remove legacy files

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,6 @@ module.exports = {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-
-    // Path to the Tremor module
     './node_modules/@tremor/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
