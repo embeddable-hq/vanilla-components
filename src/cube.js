d7ec5972b3c8b217915f8a9dae068d1259eebca5
Tom Gardiner <tom@trevor.io>
2024-03-13 22:33:40 +0100

    Add local cube:playground support for the list(...) helper function

--- src/cube.js
+++ src/cube.js
@@ -1,18 +1,23 @@
+function hashCode(obj) {
+  var str = JSON.stringify(obj);
+  var hash = 0, i = 0, len = str.length;
+  while ( i < len ) {
+      hash  = ((hash << 5) - hash + str.charCodeAt(i++)) << 0;
+  }
+  return Math.abs(hash);
+}
+
 module.exports = {
 
   contextToAppId: ({ securityContext }) => {
-    return `CUBE_APP_${securityContext.country}`;
+    return `CUBE_APP_${hashCode(securityContext)}`;
   },
 
   scheduledRefreshContexts: () => [
     {
       securityContext: {
-        country: 'United States'
-      }
-    },
-    {
-      securityContext: {
-        country: 'Germany'
+        // see:
+        // https://cube.dev/docs/reference/configuration/config#scheduled_refresh_contexts
       }
     }
   ]
