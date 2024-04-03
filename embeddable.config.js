4b5f920373d7fd5cf127551a30aeffa49ce2b7d5
rui@trevor.io <rui@embeddable.com>
2024-04-03 10:54:03 +0300

    cleanup

--- embeddable.config.js
+++ embeddable.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from '@embeddable.com/sdk-core';
 import react from '@embeddable.com/sdk-react';
 
 export default defineConfig({
-  plugins: [react],
+  plugins: [react]
 
-  previewBaseUrl: 'https://app.dev.embeddable.com',
-  pushBaseUrl: 'https://api.dev.embeddable.com',
-  audienceUrl: 'https://api.dev.embeddable.com/',
-  authDomain: 'embeddable-dev.eu.auth0.com',
-  authClientId: 'xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG'
+  // previewBaseUrl: 'https://app.dev.embeddable.com',
+  // pushBaseUrl: 'https://api.dev.embeddable.com',
+  // audienceUrl: 'https://api.dev.embeddable.com/',
+  // authDomain: 'embeddable-dev.eu.auth0.com',
+  // authClientId: 'xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG'
 });
