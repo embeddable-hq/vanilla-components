1307ee4cee6ea5e2529a874c5e62901d1e6ae287
Henry Marshall <harry@trevor.io>
2024-04-19 14:40:46 +0200

    const defined outside function

--- src/components/util/hexToRgb.ts
+++ src/components/util/hexToRgb.ts
@@ -1,10 +1,10 @@
 
 
+
+const BASE_16 = 16;
+
 export default function hexToRgb(hex: string, opacity: number): string {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-  const BASE_16 = 16;
-
   return result
     ? `rgba(${parseInt(result[1], BASE_16)}, ${parseInt(result[2], BASE_16)}, ${parseInt(
         result[3], BASE_16)}, ${opacity || 1})`
