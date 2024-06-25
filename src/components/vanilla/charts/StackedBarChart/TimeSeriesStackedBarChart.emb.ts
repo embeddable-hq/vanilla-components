45ace047ac1b68dac7f3de6839c39a21fa0b2fd9
Tom Gardiner <tom@trevor.io>
2024-06-25 20:44:39 +0200

    Add example of time-series stacked bar chart

---
+++ src/components/vanilla/charts/StackedBarChart/TimeSeriesStackedBarChart.emb.ts
@@ -0,0 +1,121 @@
+import { loadData } from '@embeddable.com/core';
+import { EmbeddedComponentMeta, Inputs, defineComponent } from '@embeddable.com/react';
+
+import Component from './index';
+
+export const meta = {
+  name: 'TimeSeriesStackedBarChart',
+  label: 'Chart: Stacked Bar (time-series)',
+  classNames: ['inside-card'],
+  inputs: [
+    {
+      name: 'title',
+      type: 'string',
+      label: 'Title',
+      description: 'The title for the chart',
+      category: 'Configure chart'
+    },
+    {
+      name: 'ds',
+      type: 'dataset',
+      label: 'Dataset to display',
+      category: 'Configure chart'
+    },
+    {
+      name: 'xAxis',
+      type: 'dimension',
+      label: 'X-Axis',
+      config: {
+        dataset: 'ds',
+        supportedTypes: ['time']
+      },
+      category: 'Configure chart'
+    },
+    {
+      name: 'granularity',
+      type: 'granularity',
+      label: 'Granularity',
+      category: 'Configure chart'
+    },
+    {
+      name: 'segment',
+      type: 'dimension',
+      label: 'Segment',
+      config: {
+        dataset: 'ds'
+      },
+      category: 'Configure chart'
+    },
+    {
+      name: 'metric',
+      type: 'measure',
+      label: 'Metric',
+      config: {
+        dataset: 'ds'
+      },
+      category: 'Configure chart'
+    },
+    {
+      name: 'showLegend',
+      type: 'boolean',
+      label: 'Show legend',
+      defaultValue: true,
+      category: 'Chart settings'
+    },
+    {
+      name: 'maxSegments',
+      type: 'number',
+      label: 'Max Legend Items',
+      defaultValue: 8,
+      category: 'Chart settings'
+    },
+    {
+      name: 'showLabels',
+      type: 'boolean',
+      label: 'Show Labels',
+      defaultValue: false,
+      category: 'Chart settings'
+    },
+    {
+      name: 'displayAsPercentage',
+      type: 'boolean',
+      label: 'Display as Percentages',
+      defaultValue: false,
+      category: 'Chart settings'
+    },
+    {
+      name: 'dps',
+      type: 'number',
+      label: 'Decimal Places',
+      category: 'Formatting'
+    },
+    {
+      name: 'enableDownloadAsCSV',
+      type: 'boolean',
+      label: 'Show download as CSV',
+      category: 'Export options',
+      defaultValue: true,
+    }
+  ]
+} as const satisfies EmbeddedComponentMeta;
+
+export default defineComponent(Component, meta, {
+  props: (inputs: Inputs<typeof meta>) => {
+    return {
+      ...inputs,
+      results: loadData({
+        from: inputs.ds,
+        timeDimensions: [
+          {
+            dimension: inputs.xAxis?.name,
+            granularity: inputs.granularity
+          }
+        ],
+        dimensions: [inputs.segment],
+        measures: [inputs.metric]
+        // orderBy: [{ property: inputs.xAxis, direction: 'desc' }],
+        // limit: 12
+      })
+    };
+  }
+});
