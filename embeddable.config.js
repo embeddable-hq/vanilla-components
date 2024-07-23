892aaee2fea86427905e74784ca0004aae9f3f1e
rui@trevor.io <rui@embeddable.com>
2024-07-23 11:13:48 +0100

    investigation

--- embeddable.config.js
+++ embeddable.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from '@embeddable.com/sdk-core';
 import react from '@embeddable.com/sdk-react';
 
 export default defineConfig({
-  plugins: [react]
-
-  // previewBaseUrl: 'https://app.dev.embeddable.com',
-  // pushBaseUrl: 'https://api.dev.embeddable.com',
-  // audienceUrl: 'https://api.dev.embeddable.com/',
-  // authDomain: 'embeddable-dev.eu.auth0.com',
-  // authClientId: 'xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG',
+  plugins: [react],
+  previewBaseUrl: 'https://app.dev.embeddable.com',
+  pushBaseUrl: 'https://api.dev.embeddable.com',
+  audienceUrl: 'https://api.dev.embeddable.com/',
+  authDomain: 'embeddable-dev.eu.auth0.com',
+  authClientId: 'xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG'
 });
--- src/components/vanilla/charts/Text/index.tsx
+++ src/components/vanilla/charts/Text/index.tsx
@@ -1,8 +1,14 @@
+// React Data Grid Component
+import 'ag-grid-community/styles/ag-grid.css';
+// Mandatory CSS required by the Data Grid
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+import { AgGridReact } from 'ag-grid-react';
+// Optional Theme applied to the Data Grid
 import React from 'react';
 
-
-
 import Container from '../../Container';
+
+// Optional Theme applied to the Data Grid
 
 type Props = {
   title: string;
@@ -10,7 +16,28 @@ type Props = {
 };
 
 export default (props: Props) => {
-  const { title, body } = props;
+  const [rowData, setRowData] = React.useState([
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false }
+  ]);
 
-  return <Container title={title}>{body}</Container>;
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = React.useState([
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' }
+  ]);
+
+  return (
+    <Container title={props.title} description={props.body}>
+      <div
+        className="ag-theme-quartz" // applying the Data Grid theme
+        style={{ height: 500 }} // the Data Grid will fill the size of the parent container
+      >
+        <AgGridReact rowData={rowData} columnDefs={colDefs} />
+      </div>
+    </Container>
+  );
 };
