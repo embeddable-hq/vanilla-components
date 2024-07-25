8d4f9a7c34f74b861da31200d22827c8b8756f73
Henry Marshall <harry@trevor.io>
2024-07-25 18:36:38 +0200

    Small refactor Bug fixed whereby changing the default xAxis input caused an error

--- src/components/vanilla/charts/DynamicAxisBar/index.tsx
+++ src/components/vanilla/charts/DynamicAxisBar/index.tsx
@@ -23,22 +23,25 @@ type Props = {
 export default (props: Props) => {
   const { results, title } = props;
 
-  const [value, setValue] = useState(props.xAxis?.title);
-  const [_, setDimension] = useEmbeddableState();
+  const [value, setValue] = useState(props.xAxis.name);
+  const [_, setDimension] = useEmbeddableState( { dimension: null } );
 
-  const xAxisOptions = props.xAxisOptions?.find((item) => props.xAxis.title == item.title) 
+  useEffect(() => {
+    setValue(props.xAxis.name);
+  }, [props.xAxis])
+
+
+  const xAxisOptions = props.xAxisOptions?.find((item) => props.xAxis.name == item.name) 
     ? props.xAxisOptions 
     : [...(props.xAxisOptions || []), props.xAxis ]
 
-
-
   const handleChange = (newValue:string) => {
     setValue(newValue);
-    const selectedDimension = xAxisOptions.find(item => newValue === item.title);
+    const selectedDimension = xAxisOptions.find(item => newValue === item.name);
     setDimension({ dimension: selectedDimension });
   }
 
-  const xAxis = xAxisOptions.find(item => value === item.title);
+  const xAxis = xAxisOptions.find(item => value === item.name);
   const updatedProps = {...props, xAxis: xAxis };
 
   return (
@@ -48,12 +51,12 @@ export default (props: Props) => {
       <div className="flex h-[60px] w-full">
           <div>
             <select className="relative px-3 rounded-xl w-full min-w-[50px] h-10 border border-[#DADCE1] flex items-center" value={value} onChange={(e) => handleChange(e.target.value)}>
-              {xAxisOptions?.map((o, i) => <option key={i} value={o.title}>{o.title}</option>)}
+              {xAxisOptions?.map((o, i) => <option key={i} value={o.name}>{o.title}</option>)}
             </select>
           </div>
         </div>
         <div className='flex grow overflow-hidden'>
-          {results.isLoading || results?.data?.[0]?.[xAxis.name] == null
+          {results.isLoading || results?.data?.[0]?.[xAxis?.name] == null
             ? null 
             : (
               <BarChart
