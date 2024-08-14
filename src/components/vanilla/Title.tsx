d8d896e622b2e115ba3c1f20535cd080c4c3768c
hjkmarshall <150246125+hjkmarshall@users.noreply.github.com>
2024-08-14 16:00:32 +0200

    Text component font size (#105)
    Co-authored-by: Henry Marshall <harry@trevor.io>

--- src/components/vanilla/Title.tsx
+++ src/components/vanilla/Title.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 type Props = {
   title?: string;
+  style?: React.CSSProperties;
 };
 
-export default function Title({ title }: Props) {
+export default function Title({ title, style }: Props) {
   return (
     !!title && (
-      <h2 className="w-full text-[#333942] text-base font-bold font-embeddable justify-start flex leading-6 mb-4">
+      <h2
+        className="w-full text-[#333942] text-base font-bold font-embeddable justify-start flex leading-6 mb-4"
+        style={style || {}}
+      >
         {title}
       </h2>
     )
