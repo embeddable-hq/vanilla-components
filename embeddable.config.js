f5c08647b3b54fff82ffd42054ba863f1307353d
Nickolas Lago <nickolas.lago@gmail.com>
2024-09-05 09:49:38 +0100

    eu-migrate

--- embeddable.config.js
+++ embeddable.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from '@embeddable.com/sdk-core';
 import react from '@embeddable.com/sdk-react';
 
 export default defineConfig({
-  plugins: [react]
+  plugins: [react],
 
-  // previewBaseUrl: 'https://app.eu.embeddable.com',
-  // pushBaseUrl: 'https://app-api.eu.embeddable.com',
-  // audienceUrl: 'https://auth.eu.embeddable.com',
-  // authDomain: 'auth.eu.embeddable.com',
-  // authClientId: '6OGPwIQsVmtrBKhNrwAaXhz4ePb0kBGV',
+  previewBaseUrl: 'https://app.eu.embeddable.com',
+  pushBaseUrl: 'https://app-api.eu.embeddable.com',
+  audienceUrl: 'https://auth.eu.embeddable.com',
+  authDomain: 'auth.eu.embeddable.com',
+  authClientId: '6OGPwIQsVmtrBKhNrwAaXhz4ePb0kBGV',
 
   // previewBaseUrl: 'https://app.dev.embeddable.com',
   // pushBaseUrl: 'https://api.dev.embeddable.com',
