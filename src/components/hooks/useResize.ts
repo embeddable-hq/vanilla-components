4702e5374a48341171a98f7f5578d401858ba69b
hjkmarshall <150246125+hjkmarshall@users.noreply.github.com>
2024-09-06 17:14:27 +0200

    Table improve (#107)
    Co-authored-by: Radovan Kuka <kuka.radovan@gmail.com>
    Co-authored-by: Henry Marshall <harry@trevor.io>

--- src/components/hooks/useResize.ts
+++ src/components/hooks/useResize.ts
@@ -1,26 +1,56 @@
-import { MutableRefObject, useLayoutEffect, useState } from 'react';
+import { useLayoutEffect, useState, useRef, MutableRefObject, useCallback } from 'react';
+import throttle from 'lodash.throttle';
 
-export default (ref: MutableRefObject<HTMLDivElement | null>) => {
-  const [width, setWidth] = useState<undefined | number>(undefined);
-  const [height, setHeight] = useState<undefined | number>(undefined);
+type ResizeOptions = {
+  delay?: number;
+}
+
+export type Size = {
+  width: number;
+  height: number;
+}
+
+const useResize = (
+  target: MutableRefObject<HTMLDivElement | null>,
+  callback: ((size: Size) => void) | null,
+  options: ResizeOptions = {}
+): Size => {
+  const [size, setSize] = useState<Size>({
+    width: 0,
+    height: 0,
+  });
+  const resizeObserver = useRef<ResizeObserver | null>(null);
+  const callbackRef = useRef(callback);
+
+  const resizeHandler = useCallback((entries: ResizeObserverEntry[]) => {
+    if (entries[0]?.contentRect) {
+      const actualSize = {
+        width: entries[0]?.contentRect.width,
+        height: entries[0]?.contentRect.height,
+      }
+
+      setSize(actualSize);
+
+      callbackRef.current?.(actualSize);
+    }
+  }, [callbackRef]);
 
   useLayoutEffect(() => {
-    const ro = new ResizeObserver((entries) => {
-      if (entries[0]?.contentRect.width) {
-        setWidth(entries[0]?.contentRect.width);
-      }
+    const targetElement = target && 'current' in target ? target.current : target;
 
-      if (entries[0]?.contentRect.height) {
-        setHeight(entries[0].contentRect.height);
-      }
-    });
+    if (!targetElement) {
+      return;
+    }
 
-    if (ref?.current) ro.observe(ref?.current);
+    resizeObserver.current = new ResizeObserver(throttle(resizeHandler, options?.delay ?? 0));
+    resizeObserver.current.observe(targetElement);
 
     return () => {
-      ro.disconnect();
-    };
-  }, [ref]);
+      resizeObserver.current?.unobserve(targetElement);
+    }
+  }, [target, resizeHandler, options?.delay]);
 
-  return [width, height];
+  return size
 };
+
+export default useResize;
--- src/components/vanilla/Container.tsx
+++ src/components/vanilla/Container.tsx
@@ -1,9 +1,9 @@
 import { DataResponse } from '@embeddable.com/core';
-import React, { PropsWithChildren, useRef, useState } from 'react';
+import React, { PropsWithChildren, useRef, useState, useEffect } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 import useFont from '../hooks/useFont';
-import useResize from '../hooks/useResize';
+import useResize, { Size } from '../hooks/useResize';
 import Spinner from './Spinner';
 import DownloadIcon from './DownloadIcon';
 import Title from './Title';
@@ -14,22 +14,60 @@ import './index.css';
 type Props = {
   description?: string;
   className?: string;
+  childContainerClassName?: string;
   title?: string;
   results?: DataResponse | DataResponse[];
   enableDownloadAsCSV?: boolean;
+  onResize?: (size: Size) => void;
+  setResizeState?: (resizing: boolean) => void;
 }
 
-export default ({ children, className, ...props }: PropsWithChildren<Props>) => {
+export default ({ children, className, childContainerClassName, onResize, setResizeState, ...props  }: PropsWithChildren<Props>) => {
   const ref = useRef<HTMLDivElement | null>(null);
-  const [width, height] = useResize(ref);
+  const prevHeightRef = useRef<number | null>(null);
+  const resizingTimeoutRef = useRef<number | null>(null);
+
+  const { height } = useResize(ref, onResize || null);
+
+  //Detect when the component is being resized by the user
+  useEffect(() => {
+    if(!setResizeState) {
+      return;
+    }
+    const currentHeight = height;
+    // If prevHeightRef is null, this is the first render, so initialize it
+    if (prevHeightRef.current === null) {
+      prevHeightRef.current = currentHeight;
+    }
+    if (currentHeight !== prevHeightRef.current) {
+      setResizeState?.(true);
+      // Clear the timeout if it exists, to debounce the resize state reset
+      if (resizingTimeoutRef.current) {
+       window.clearTimeout(resizingTimeoutRef.current);
+      }
+      // Set a timer to reset the resize state after 300ms
+      resizingTimeoutRef.current = window.setTimeout(() => {
+        setResizeState?.(false);
+      }, 300);
+    }
+    // Update the previous height with the current height
+    prevHeightRef.current = currentHeight;
+    // Clean up the timeout when the component unmounts
+    return () => {
+      if (resizingTimeoutRef.current) {
+        window.clearTimeout(resizingTimeoutRef.current);
+      }
+    };
+  }, [height]); // Depend on height, so it runs whenever height changes
+
   const [preppingDownload, setPreppingDownload] = useState(false);
   const { isLoading, error, data } = Array.isArray(props.results) ? {
     isLoading: props.results.some((result) => result.isLoading),
     error: props.results.some((result) => result.error),
-    data: props.results.flatMap((result) => result.data),
+    data: props.results.flatMap((result) => result.data).filter((data) => data),
   } : props.results || {};
   const noData = !isLoading && !data?.length;
-
+  
   useFont();
 
   if (props.results && (error || noData)) {
@@ -60,13 +98,20 @@ export default ({ children, className, ...props }: PropsWithChildren<Props>) => 
       <Spinner show={isLoading || preppingDownload} />
       <Title title={props.title} />
       <Description description={props.description} />
-      <div className={twMerge(`relative grow flex flex-col`, className || '')} ref={ref}>
+
+      <div
+        ref={ref}
+        className={twMerge(`relative grow flex flex-col`, className || '')}
+      >
         {!!height && (
-          <div className="flex flex-col" style={{ height: `${height}px` }}>
+          <div 
+            className={twMerge(`flex flex-col`, childContainerClassName || '')}
+            style={{ height: `${height}px` }}
+          >
             {children}
           </div>
         )}
-        {isLoading && (
+        {isLoading && !data?.length && (
           <div className="absolute left-0 top-0 w-full h-full z-10 skeleton-box bg-gray-300 overflow-hidden rounded" />
         )}
       </div>
