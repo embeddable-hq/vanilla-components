98b52ef7c9bedd7edc7be8a6ea36d676f6392e22
Christopher Buecheler <chris@embeddable.com>
2024-10-21 13:42:51 +0200

    clean up TS errors in a few controls

--- src/components/vanilla/controls/DatePicker/utils/dateUtils.ts
+++ src/components/vanilla/controls/DatePicker/utils/dateUtils.ts
@@ -1,20 +1,34 @@
-import { differenceInSeconds, differenceInCalendarDays, getYear, format, subDays, subMonths, subQuarters, subYears } from 'date-fns';
+import { Granularity } from '@embeddable.com/core';
+import {
+  differenceInCalendarDays,
+  differenceInSeconds,
+  format,
+  getYear,
+  subDays,
+  subMonths,
+  subQuarters,
+  subYears,
+} from 'date-fns';
 
-
-function toSeconds (unit: string, n: number): number {
-  const unitsInSeconds = {
-      minute: 60,
-      hour: 3600,
-      day: 86400,
-      week: 604800,
-      month: 2629800,  // Roughly 30.44 days
-      quarter: 7889400,  // Roughly 91.31 days
-      year: 31557600  // Based on a typical Gregorian year
+function toSeconds(unit: string, n: number): number {
+  const unitsInSeconds: { [key: string]: number } = {
+    minute: 60,
+    hour: 3600,
+    day: 86400,
+    week: 604800,
+    month: 2629800, // Roughly 30.44 days
+    quarter: 7889400, // Roughly 91.31 days
+    year: 31557600, // Based on a typical Gregorian year
   };
   return n * unitsInSeconds[unit];
 }
 
-export function getComparisonOptions(period) {
+type TimeRange = {
+  from: Date;
+  to: Date;
+};
+
+export function getComparisonOptions(period: TimeRange) {
   if (!period?.from || !period?.to) {
     return [{ value: 'No comparison' }];
   }
@@ -25,55 +39,58 @@ export function getComparisonOptions(period) {
     { value: 'No comparison' },
     {
       value: 'Previous period',
-      note: getNote(subDays(period.from, days), subDays(period.to, days))
+      note: getNote(subDays(period.from, days), subDays(period.to, days)),
     },
     {
       value: 'Previous month',
-      note: getNote(subMonths(period.from, 1), subMonths(period.to, 1))
+      note: getNote(subMonths(period.from, 1), subMonths(period.to, 1)),
     },
     {
       value: 'Previous quarter',
-      note: getNote(subQuarters(period.from, 1), subQuarters(period.to, 1))
+      note: getNote(subQuarters(period.from, 1), subQuarters(period.to, 1)),
     },
     {
       value: 'Previous year',
-      note: getNote(subYears(period.from, 1), subYears(period.to, 1))
-    }
+      note: getNote(subYears(period.from, 1), subYears(period.to, 1)),
+    },
   ];
 }
 
-export function getComparisonPeriod(rts, period) {
-	if (rts === 'Previous month') {
-		return {
-			relativeTimeString: 'previous month',
-			from: subMonths(period.from, 1),
-			to: subMonths(period.to, 1) 
-		}
-	} else if (rts ===  'Previous quarter') {
-		return {
-			relativeTimeString: 'previous quarter',
-			from: subQuarters(period.from, 1),
-			to: subQuarters(period.to, 1)
-		}
-	} else if ( rts === 'Previous year') {
-		return {
-			relativeTimeString: 'previous year',
-			from: subYears(period.from, 1),
-			to: subYears(period.to, 1)
-		}
-	} else {// Previous period
-		const days = Math.abs(differenceInCalendarDays(period.from, period.to)) + 1;
-		return {
-			relativeTimeString: 'previous period',
-			from: subDays(period.from, days),
-			to: subDays(period.to, days)
-		}
-	}
+export function getComparisonPeriod(rts: string, period: TimeRange) {
+  if (rts === 'Previous month') {
+    return {
+      relativeTimeString: 'previous month',
+      from: subMonths(period.from, 1),
+      to: subMonths(period.to, 1),
+    };
+  } else if (rts === 'Previous quarter') {
+    return {
+      relativeTimeString: 'previous quarter',
+      from: subQuarters(period.from, 1),
+      to: subQuarters(period.to, 1),
+    };
+  } else if (rts === 'Previous year') {
+    return {
+      relativeTimeString: 'previous year',
+      from: subYears(period.from, 1),
+      to: subYears(period.to, 1),
+    };
+  } else {
+    // Previous period
+    const days = Math.abs(differenceInCalendarDays(period.from, period.to)) + 1;
+    return {
+      relativeTimeString: 'previous period',
+      from: subDays(period.from, days),
+      to: subDays(period.to, days),
+    };
+  }
 }
 
 export function getNote(from: Date, to: Date) {
-
-  const formattedFrom = format(from, getYear(from) === getYear(new Date()) ? 'd MMM' : 'd MMM yyyy');
+  const formattedFrom = format(
+    from,
+    getYear(from) === getYear(new Date()) ? 'd MMM' : 'd MMM yyyy',
+  );
 
   const formattedTo = format(to, getYear(to) === getYear(new Date()) ? 'd MMM' : 'd MMM yyyy');
 
@@ -81,14 +98,20 @@ export function getNote(from: Date, to: Date) {
     return formattedFrom;
   } else {
     return `${formattedFrom} - ${formattedTo}`;
-  }	
+  }
 }
 
-export function getValidGranularities(period?: TimeRange): DataResponse {
+type GranularityResponse = {
+  isLoading: boolean;
+  data: { value: string }[];
+  recommended: { value: Granularity };
+};
+
+export function getValidGranularities(period?: TimeRange): GranularityResponse {
   const data: { value: Granularity }[] = [];
-  
+
   //period boundaries for valid granularity options
-  const gSettings = {
+  const gSettings: { [key: string]: { min: number; max: number } } = {
     second: { min: 2, max: 100 },
     minute: { min: toSeconds('minute', 2), max: toSeconds('minute', 100) },
     hour: { min: toSeconds('hour', 2), max: toSeconds('hour', 100) },
@@ -96,8 +119,8 @@ export function getValidGranularities(period?: TimeRange): DataResponse {
     week: { min: toSeconds('week', 2), max: toSeconds('week', 365) },
     month: { min: toSeconds('month', 2), max: toSeconds('month', 730) },
     quarter: { min: toSeconds('quarter', 2), max: toSeconds('quarter', 36) },
-    year: { min: toSeconds('year', 1.5), max: toSeconds('year', 1000) }
-  }
+    year: { min: toSeconds('year', 1.5), max: toSeconds('year', 1000) },
+  };
 
   const granularities = Object.keys(gSettings);
   const diff = differenceInSeconds(period?.to || new Date(), period?.from || new Date());
@@ -108,12 +131,12 @@ export function getValidGranularities(period?: TimeRange): DataResponse {
     data.push({ value: value as Granularity });
   });
 
-  let recommendedG = { value: 'day' };
+  let recommendedG: { value: Granularity } = { value: 'day' };
   if (data.length >= 2) recommendedG = data[data.length - 2]; //set the recommended granularity option as the penultimate valid option.
 
   return {
     isLoading: false,
     data,
-    recommended: recommendedG
+    recommended: recommendedG,
   };
 }
