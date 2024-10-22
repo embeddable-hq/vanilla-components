511afb9a4b23ad8de3e125d9c3d2f2bb5c48ae21
Christopher Buecheler <chris@embeddable.com>
2024-10-22 12:40:31 +0200

    Fix null value filter errors, orderby errors, a few others

--- src/components/vanilla/charts/DynamicAxisBar/DynamicAxisBar.emb.ts
+++ src/components/vanilla/charts/DynamicAxisBar/DynamicAxisBar.emb.ts
@@ -1,7 +1,7 @@
+import { DimensionOrMeasure, loadData } from '@embeddable.com/core';
 import { EmbeddedComponentMeta, Inputs, defineComponent } from '@embeddable.com/react';
-import { loadData } from '@embeddable.com/core';
 
-import Component from './index';
+import Component, { Props } from './index';
 
 export const meta = {
   name: 'DynamicAxisBar',
@@ -10,151 +10,163 @@ export const meta = {
   category: 'Misc',
   inputs: [
     {
-        name: 'ds',
-        type: 'dataset',
-        label: 'Dataset to download from',
-        category: 'Chart data'
+      name: 'ds',
+      type: 'dataset',
+      label: 'Dataset to download from',
+      category: 'Chart data',
     },
     {
-        name: 'xAxis',
-        type: 'dimension',
-        label: 'Default X-axis',
-        config: {
-            dataset: 'ds',
-        },
-        category: 'Chart data'
+      name: 'xAxis',
+      type: 'dimension',
+      label: 'Default X-axis',
+      config: {
+        dataset: 'ds',
+      },
+      category: 'Chart data',
     },
     {
-        name: 'xAxisOptions',
-        type: 'dimension',
-        array: true,
-        label: 'X-axis options',
-        config: {
-            dataset: 'ds'
-        },
-        category: 'Chart data'
+      name: 'xAxisOptions',
+      type: 'dimension',
+      array: true,
+      label: 'X-axis options',
+      config: {
+        dataset: 'ds',
+      },
+      category: 'Chart data',
     },
     {
-        name: 'metrics',
-        type: 'measure',
-        array: true,
-        label: 'Metrics',
-        config: {
-            dataset: 'ds'
-        },
-        category: 'Chart data'
+      name: 'metrics',
+      type: 'measure',
+      array: true,
+      label: 'Metrics',
+      config: {
+        dataset: 'ds',
+      },
+      category: 'Chart data',
     },
     {
       name: 'granularity',
       type: 'granularity',
       label: 'Granularity (for dates)',
       defaultValue: 'week',
-      category: 'Variables to configure'
+      category: 'Variables to configure',
     },
     {
-        name: 'title',
-        type: 'string',
-        label: 'Title',
-        description: 'The title for the button',
-        category: 'Chart settings'
+      name: 'title',
+      type: 'string',
+      label: 'Title',
+      description: 'The title for the button',
+      category: 'Chart settings',
     },
     {
-        name: 'description',
-        type: 'string',
-        label: 'Description',
-        description: 'The description for the button',
-        category: 'Chart settings'
+      name: 'description',
+      type: 'string',
+      label: 'Description',
+      description: 'The description for the button',
+      category: 'Chart settings',
     },
     {
       name: 'showLegend',
       type: 'boolean',
       label: 'Show Legend',
       category: 'Chart settings',
-      defaultValue: true
+      defaultValue: true,
     },
     {
       name: 'showLabels',
       type: 'boolean',
       label: 'Show Labels',
       category: 'Chart settings',
-      defaultValue: false
+      defaultValue: false,
     },
     {
       name: 'displayHorizontally',
       type: 'boolean',
       label: 'Display Horizontally',
       category: 'Chart settings',
-      defaultValue: false
+      defaultValue: false,
     },
     {
       name: 'reverseXAxis',
       type: 'boolean',
       label: 'Reverse X Axis',
       category: 'Chart settings',
-      defaultValue: false
+      defaultValue: false,
     },
     {
       name: 'dps',
       type: 'number',
       label: 'Decimal Places',
-      category: 'Formatting'
+      category: 'Formatting',
     },
     {
       name: 'enableDownloadAsCSV',
       type: 'boolean',
       label: 'Show download as CSV',
       category: 'Export options',
-      defaultValue: true
-    }
-  ]
+      defaultValue: true,
+    },
+  ],
 } as const satisfies EmbeddedComponentMeta;
 
-export default defineComponent<Props, typeof meta, { page: number }>(Component, meta, {
-  props: (inputs: Inputs<typeof meta>, [embState]) => {
+type EmbeddableState = {
+  page: number;
+  dimension?: {
+    title: string;
+    nativeType: 'string' | 'number' | 'boolean' | 'time';
+    name: string;
+    __type__: 'dimension';
+  };
+};
 
-    const selectedDimension = embState?.dimension?.title ? embState.dimension : inputs.xAxis;
+export default defineComponent<Props, typeof meta, EmbeddableState>(Component, meta, {
+  props: (inputs: Inputs<typeof meta>, [embState]) => {
+    const selectedDimension: DimensionOrMeasure = embState?.dimension?.title
+      ? embState.dimension
+      : inputs.xAxis;
 
     const isTimeDimension = selectedDimension.nativeType === 'time';
-    
 
     return {
-        ...inputs,
-        reverseXAxis: inputs.reverseXAxis || isTimeDimension,
-        results: isTimeDimension 
-            ? loadData({
-                from: inputs.ds,
-                timeDimensions: [
-                  {
-                    dimension: selectedDimension.name,
-                    granularity: inputs.granularity
-                  }
-                ],
-                measures: inputs.metrics,
-                filters: [{
-                    property: selectedDimension,
-                    operator: 'notEquals',
-                    value: [null]
-                }],
-                orderBy: [
-                    { 
-                        property: selectedDimension, 
-                        direction: 'desc' 
-                    }
-                ],
-                limit: 100
-            }) 
-            : loadData({
-                from: inputs.ds,
-                dimensions: [selectedDimension],
-                measures: inputs.metrics,
-                orderBy: [
-                    {
-                        property: inputs.metrics[0],
-                        direction: 'desc'
-                    }
-                ],
-                limit: 100
-            })
+      ...inputs,
+      reverseXAxis: inputs.reverseXAxis || isTimeDimension,
+      results: isTimeDimension
+        ? loadData({
+            from: inputs.ds,
+            timeDimensions: [
+              {
+                dimension: selectedDimension.name,
+                granularity: inputs.granularity,
+              },
+            ],
+            measures: inputs.metrics,
+            filters: [
+              {
+                property: selectedDimension,
+                operator: 'notEquals',
+                value: [0],
+              },
+            ],
+            orderBy: [
+              {
+                property: selectedDimension,
+                direction: 'desc',
+              },
+            ],
+            limit: 100,
+          })
+        : loadData({
+            from: inputs.ds,
+            dimensions: [selectedDimension],
+            measures: inputs.metrics,
+            orderBy: [
+              {
+                property: inputs.metrics[0],
+                direction: 'desc',
+              },
+            ],
+            limit: 100,
+          }),
     };
-  }
+  },
 });
