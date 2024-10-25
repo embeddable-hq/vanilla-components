6d08770618e2d1bed5c863b051bf7d8ddb5ff45e
Christopher Buecheler <cwbuecheler@users.noreply.github.com>
2024-10-25 11:56:49 +0200

    Fix TypeScript Errors in Vanilla Components (#134)
    Co-authored-by: Christopher Buecheler <christopherbuecheler@Chris---Embeddable---MBP.local>
    Co-authored-by: Christopher Buecheler <chris@embeddable.com>

--- src/components/util/downloadAsCSV.ts
+++ src/components/util/downloadAsCSV.ts
@@ -1,64 +1,91 @@
-export default (props, data, prevData, setLoadingState) => {
+type DataResponseData = { [key: string]: any };
 
-	if (setLoadingState) setLoadingState(true);
+type Input = {
+  [key: string]: any;
+};
 
-	const titlesByName = (() => {
-		const titles = {};
-		const extractTitle = input => {
-			if(['measure', 'dimension'].includes(input?.__type__)) {
-				titles[input.name] = input.title;
-			}
-		}
-		for (const [key, value] of Object.entries(props)) {
-			if(Array.isArray(value)) {
-				value.forEach(extractTitle)
-			} else {
-				extractTitle(value);
-			}
-		}
-		console.log('titles', titles)
-		return titles;
-	})();
+type Props = {
+  [key: string]: any;
+};
 
-	const arrayToCsv = (data) => {
-		return data.map(row =>
-			row
-				.map(String)  // convert every value to String
-				.map(v => v.replaceAll('"', '""'))  // escape double quotes
-				.map(v => `"${v}"`)  // quote it
-				.join(',')  // comma-separated
-			).join('\r\n');  // rows starting on new lines
-	}
+const downloadAsCSV = (
+  props: Props,
+  data: DataResponseData[] | undefined,
+  prevData: DataResponseData[] | undefined,
+  setLoadingState?: React.Dispatch<React.SetStateAction<boolean>>,
+) => {
+  if (setLoadingState) {
+    setLoadingState(true);
+  }
 
-	const downloadBlob = (content, filename, contentType) => {
-		// Create a blob
-		const blob = new Blob([content], { type: contentType });
-		const url = URL.createObjectURL(blob);
+  // If there's no data, we can't export anything
+  if (!data) {
+    if (setLoadingState) setLoadingState(false);
+    return;
+  }
 
-		// Create a link to download it
-		const pom = document.createElement('a');
-		pom.href = url;
-		pom.setAttribute('download', filename);
-		pom.click();
-		if (setLoadingState) setLoadingState(false);
-	}
+  const titlesByName = (() => {
+    const titles: { [key: string]: string } = {};
+    const extractTitle = (input: Input) => {
+      if (['measure', 'dimension'].includes(input?.__type__)) {
+        titles[input.name] = input.title;
+      }
+    };
+    for (const [key, value] of Object.entries(props)) {
+      if (Array.isArray(value)) {
+        value.forEach(extractTitle);
+      } else {
+        extractTitle(value);
+      }
+    }
+    return titles;
+  })();
 
-	const prepCSV = () => {
-		const rows = [];
-		const columns = Object.keys(data[0]);
-		rows.push(columns.map(c => titlesByName[c] || c)); //title row
+  const arrayToCsv = (arrayData: any[][]) => {
+    return arrayData
+      .map(
+        (row) =>
+          row
+            .map(String) // convert every value to String
+            .map((v) => v.replaceAll('"', '""')) // escape double quotes
+            .map((v) => `"${v}"`) // quote it
+            .join(','), // comma-separated
+      )
+      .join('\r\n'); // rows starting on new lines
+  };
 
-		data?.map(row => {
-			const rowValues = columns.map(c => row[c]);
-			rows.push(rowValues);
-		})
-		prevData?.map(row => {
-			const rowValues = columns.map(c => row[c]);
-			rows.push(rowValues);
-		})
-		return arrayToCsv(rows);
-	}
-   
-    return setTimeout(() => {  downloadBlob(prepCSV(), 'export.csv', 'text/csv;charset=utf-8;'); }, 200); //timeout set to indicate that the download is in progress (if instant it can appear to the user that it hasn't been successful)
+  const downloadBlob = (content: string, filename: string, contentType: string) => {
+    // Create a blob
+    const blob = new Blob([content], { type: contentType });
+    const url = URL.createObjectURL(blob);
 
-}
+    // Create a link to download it
+    const pom = document.createElement('a');
+    pom.href = url;
+    pom.setAttribute('download', filename);
+    pom.click();
+    if (setLoadingState) setLoadingState(false);
+  };
+
+  const prepCSV = () => {
+    const rows = [];
+    const columns = Object.keys(data[0]);
+    rows.push(columns.map((c) => titlesByName[c] || c)); //title row
+
+    data?.map((row) => {
+      const rowValues = columns.map((c) => row[c]);
+      rows.push(rowValues);
+    });
+    prevData?.map((row) => {
+      const rowValues = columns.map((c) => row[c]);
+      rows.push(rowValues);
+    });
+    return arrayToCsv(rows);
+  };
+
+  return setTimeout(() => {
+    downloadBlob(prepCSV(), 'export.csv', 'text/csv;charset=utf-8;');
+  }, 200); //timeout set to indicate that the download is in progress (if instant it can appear to the user that it hasn't been successful)
+};
+
+export default downloadAsCSV;
