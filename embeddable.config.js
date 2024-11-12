54a6bac3a1db0c8de6d3d0bef24465c783001c11
Tom Gardiner <tom@trevor.io>
2024-11-12 13:05:38 +0100

    add US/EU instructions to readme

--- embeddable.config.js
+++ embeddable.config.js
@@ -4,12 +4,24 @@ import react from '@embeddable.com/sdk-react';
 export default defineConfig({
   plugins: [react],
 
+  // 
+  // Uncomment for US deployments
+  //
+  // previewBaseUrl: 'https://app.us.embeddable.com',
+  // pushBaseUrl: 'https://api.us.embeddable.com',
+  
+  //
+  // Uncomment for EU deployments
+  //
   // previewBaseUrl: 'https://app.eu.embeddable.com',
   // pushBaseUrl: 'https://api.eu.embeddable.com',
   // audienceUrl: 'https://auth.eu.embeddable.com',
   // authDomain: 'auth.eu.embeddable.com',
   // authClientId: '6OGPwIQsVmtrBKhNrwAaXhz4ePb0kBGV',
 
+  //
+  // For internal use only
+  //
   // previewBaseUrl: 'https://app.dev.embeddable.com',
   // pushBaseUrl: 'https://api.dev.embeddable.com',
   // audienceUrl: 'https://api.dev.embeddable.com/',
