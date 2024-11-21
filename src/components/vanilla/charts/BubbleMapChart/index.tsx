9f6a619559f4eb237ce16f753da7a618b945ef1c
Christopher Buecheler <chris@embeddable.com>
2024-11-21 11:40:46 +0100

    get individual pins working, adjust bubble sized based on count

--- src/components/vanilla/charts/BubbleMapChart/index.tsx
+++ src/components/vanilla/charts/BubbleMapChart/index.tsx
@@ -1,7 +1,8 @@
 // React & 3rd Party
 import React, { useEffect, useRef, useState } from 'react';
 import { Dataset } from '@embeddable.com/core';
-import Leaflet from 'leaflet';
+import Leaflet, { Marker, MarkerCluster } from 'leaflet';
+import BasicCircle from '../../../icons/BasicCircle';
 // import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 // import MarkerClusterGroup from 'react-leaflet-markercluster';
 
@@ -19,10 +20,7 @@ type Props = {
   enableDownloadAsPNG?: boolean;
 };
 
-const defaultColor = '#EBEBEB';
-const hoverColor = '#0957cb';
 const markerColor = '#8593e5';
-const position = [51.505, -0.09];
 const mapStyle = { height: '100%', minHeight: '100px' };
 
 export default (props: Props) => {
@@ -37,6 +35,7 @@ export default (props: Props) => {
   }
 
   const [mapComponents, setMapComponents] = useState<{
+    Circle: React.ComponentType<any>;
     MapContainer: React.ComponentType<any>;
     Marker: React.ComponentType<any>;
     MarkerClusterGroup: React.ComponentType<any>;
@@ -48,45 +47,66 @@ export default (props: Props) => {
   useEffect(() => {
     // Dynamically import 'react-simple-maps' components
     const loadMapComponents = async () => {
-      const { MapContainer, Marker, TileLayer } = await import('react-leaflet');
+      const { Circle, MapContainer, Marker, TileLayer } = await import('react-leaflet');
       const mc = await import('react-leaflet-markercluster');
 
-      setMapComponents({ MapContainer, Marker, TileLayer, MarkerClusterGroup: mc.default });
+      setMapComponents({ Circle, MapContainer, Marker, TileLayer, MarkerClusterGroup: mc.default });
     };
     loadMapComponents();
   }, []);
 
   if (!mapComponents) return <div>Loading map...</div>;
 
-  const { MapContainer, Marker, TileLayer, MarkerClusterGroup } = mapComponents;
+  const createClusterCustomIcon = function (cluster: MarkerCluster) {
+    const bubbleSize =
+      cluster.getChildCount() < 10
+        ? 18
+        : Math.ceil(18 * (cluster.getChildCount().toString().length * 0.66));
+    const bubbleRadius = Math.floor(bubbleSize / 2);
+    return Leaflet.divIcon({
+      html: `<span style="display: flex; width: ${bubbleSize}px; height: ${bubbleSize}px; background-color:${markerColor}; border-radius: ${bubbleRadius}px; color: #ffffff; align-items: center; justify-content: center;">${cluster.getChildCount()}</span>`,
+      className: 'custom-marker-cluster',
+      iconSize: Leaflet.point(33, 33, true),
+    });
+  };
+
+  const createCustomIcon = function () {
+    return Leaflet.divIcon({
+      html: `<span style="display: flex; width: 10px; height: 10px; background-color:${markerColor}; border-radius: 5px; color: #ffffff; align-items: center; justify-content: center;">&nbsp;</span>`,
+      className: 'custom-marker',
+      iconSize: Leaflet.point(10, 10, true),
+    });
+  };
+
+  const { Circle, MapContainer, Marker, TileLayer, MarkerClusterGroup } = mapComponents;
 
   return (
-    <div style={mapStyle}>
-      <MapContainer center={[51.509865, -0.118092]} zoom={4} style={mapStyle} maxZoom={16}>
-        <TileLayer
-          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <MarkerClusterGroup
-          onClick={(e: any) => console.log('onClick', e)}
-          maxClusterRadius={150}
-          spiderfyOnMaxZoom={true}
-          polygonOptions={{
-            fillColor: '#ffffff',
-            color: '#f00800',
-            weight: 5,
-            opacity: 1,
-            fillOpacity: 0.8,
-          }}
-          showCoverageOnHover={true}
-        >
-          {markers.map((address, index) => {
-            const lat = address.position.lat;
-            const lng = address.position.lng;
-            return <Marker key={`${lat}-${index}-${lng}`} position={[lat, lng]} title=""></Marker>;
-          })}
-        </MarkerClusterGroup>
-      </MapContainer>
-    </div>
+    <Container {...props} className="overflow-y-hidden">
+      <div style={mapStyle}>
+        <MapContainer center={[51.509865, -0.118092]} zoom={4} style={mapStyle} maxZoom={16}>
+          <TileLayer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png" attribution="" />
+          <MarkerClusterGroup
+            onClick={(e: any) => console.log('onClick', e)}
+            iconCreateFunction={createClusterCustomIcon}
+            maxClusterRadius={100}
+            spiderfyOnMaxZoom={true}
+            showCoverageOnHover={false}
+          >
+            {markers.map((address, index) => {
+              const lat = address.position.lat;
+              const lng = address.position.lng;
+              return (
+                <Marker
+                  icon={createCustomIcon()}
+                  key={`${lat}-${index}-${lng}`}
+                  position={[lat, lng]}
+                  title=""
+                ></Marker>
+              );
+            })}
+          </MarkerClusterGroup>
+        </MapContainer>
+      </div>
+    </Container>
   );
 };
