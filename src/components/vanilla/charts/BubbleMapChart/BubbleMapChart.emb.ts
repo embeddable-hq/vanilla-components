837a867431604da9e4f7877df917acfcdd029e5c
Christopher Buecheler <cwbuecheler@users.noreply.github.com>
2024-12-12 10:27:38 -0500

    TPS-454: Bubble Map Chart (#177)
    Co-authored-by: Christopher Buecheler <chris@embeddable.com>

---
+++ src/components/vanilla/charts/BubbleMapChart/BubbleMapChart.emb.ts
@@ -0,0 +1,124 @@
+import { loadData } from '@embeddable.com/core';
+import { EmbeddedComponentMeta, Inputs, defineComponent } from '@embeddable.com/react';
+
+import Component from './index';
+
+export const meta = {
+  name: 'BubbleMapChart',
+  label: 'Bubble Map Chart',
+  classNames: ['inside-card'],
+  category: 'Charts: essentials',
+  inputs: [
+    {
+      name: 'ds',
+      type: 'dataset',
+      label: 'Map Dataset',
+      description: 'Dataset',
+      defaultValue: false,
+      category: 'Chart Data',
+    },
+    {
+      name: 'bubblePlacement',
+      type: 'dimension',
+      label: 'Bubble Placement - Geolocation',
+      config: {
+        dataset: 'ds',
+      },
+      category: 'Chart Data',
+    },
+    {
+      name: 'metric',
+      type: 'measure',
+      label: 'Metric',
+      description: 'Metric to be displayed in the tooltip when a marker is hovered over',
+      config: {
+        dataset: 'ds',
+      },
+      category: 'Chart Data',
+    },
+    {
+      name: 'clusterRadius',
+      type: 'number',
+      label: 'Cluster Radius',
+      description:
+        'Radius within which markers are clustered. Larger value = fewer clusters with more items - REQUIRES REFRESH',
+      defaultValue: 20,
+      category: 'Chart Settings',
+    },
+    {
+      name: 'markerColor',
+      type: 'string',
+      label: 'Marker Color',
+      description: 'Color of the markers',
+      defaultValue: '#6574C8',
+      category: 'Chart Settings',
+    },
+    {
+      name: 'marketTextColor',
+      type: 'string',
+      label: 'Marker Text Color',
+      description: 'Color of the marker text',
+      defaultValue: '#ffffff',
+      category: 'Chart Settings',
+    },
+    {
+      name: 'markerClusterColor',
+      type: 'string',
+      label: 'Marker Cluster Color',
+      description: 'Color of the marker clusters',
+      defaultValue: '#8593e5',
+      category: 'Chart Settings',
+    },
+    {
+      name: 'markerClusterTextColor',
+      type: 'string',
+      label: 'Marker Cluster Text Color',
+      description: 'Color of the marker cluster text',
+      defaultValue: '#ffffff',
+      category: 'Chart Settings',
+    },
+    {
+      name: 'showTooltips',
+      type: 'boolean',
+      label: 'Show tooltips',
+      defaultValue: true,
+      category: 'Chart Settings',
+    },
+    {
+      name: 'customTileSet',
+      type: 'string',
+      label: 'Custom Tile Set',
+      description: 'URL of custom tile set',
+      category: 'Chart Settings',
+      defaultValue: '',
+    },
+    {
+      name: 'enableDownloadAsCSV',
+      type: 'boolean',
+      label: 'Show download as CSV',
+      category: 'Export options',
+      defaultValue: true,
+    },
+    {
+      name: 'enableDownloadAsPNG',
+      type: 'boolean',
+      label: 'Show download as PNG',
+      category: 'Export options',
+      defaultValue: true,
+    },
+  ],
+} as const satisfies EmbeddedComponentMeta;
+
+export default defineComponent(Component, meta, {
+  props: (inputs: Inputs<typeof meta>) => {
+    return {
+      ...inputs,
+      results: loadData({
+        from: inputs.ds,
+        limit: 5000,
+        dimensions: [inputs.bubblePlacement],
+        measures: [inputs.metric],
+      }),
+    };
+  },
+});
---
+++ src/components/vanilla/charts/BubbleMapChart/index.tsx
@@ -0,0 +1,206 @@
+// React & 3rd Party
+import React, { useEffect, useState } from 'react';
+import {
+  DataResponse,
+  Dataset,
+  Dimension,
+  DimensionOrMeasure,
+  Measure,
+} from '@embeddable.com/core';
+import Leaflet, { LatLngBoundsExpression, MarkerCluster } from 'leaflet';
+import { MapContainerProps, MarkerProps, TileLayerProps, Tooltip, useMap } from 'react-leaflet';
+
+// Style
+import 'leaflet/dist/leaflet.css';
+
+// Local
+import Container from '../../Container';
+
+// Constants
+const mapStyle = { height: '100%', minHeight: '100px' };
+const bounds: LatLngBoundsExpression = [
+  [0, 0],
+  [0, 0],
+]; // this gets overwritten by SetBounds
+
+type BoundsProps = {
+  markers: MarkerData[];
+};
+
+type MarkerData = {
+  count: number;
+  lat: number;
+  lng: number;
+  title: string;
+};
+
+type MetricExtended = Measure & { modelTitle: string };
+
+type Props = {
+  bubblePlacement: Dimension;
+  clusterRadius?: number;
+  customTileSet?: string;
+  ds?: Dataset;
+  enableDownloadAsCSV?: boolean;
+  enableDownloadAsPNG?: boolean;
+  markerClusterColor?: string;
+  markerColor?: string;
+  metric?: Measure;
+  results: DataResponse;
+  showTooltips?: boolean;
+};
+
+// Child Component - used to auto-zoom the map to fit all markers (rerenders if they change)
+const SetBounds = (props: BoundsProps) => {
+  const { markers } = props;
+  const map = useMap();
+  if (!markers || markers.length === 0) return null;
+
+  const bounds = markers.reduce(
+    (acc: any, marker: MarkerData) => {
+      const { lat, lng } = marker;
+      return [
+        [Math.min(acc[0][0], lat), Math.min(acc[0][1], lng)],
+        [Math.max(acc[1][0], lat), Math.max(acc[1][1], lng)],
+      ] as LatLngBoundsExpression;
+    },
+    [
+      [Infinity, Infinity],
+      [-Infinity, -Infinity],
+    ],
+  );
+  map.fitBounds(bounds);
+
+  return null;
+};
+
+// Main Component
+export default (props: Props) => {
+  const {
+    bubblePlacement,
+    clusterRadius,
+    customTileSet,
+    ds,
+    markerClusterColor,
+    markerColor,
+    metric,
+    results,
+    showTooltips,
+  } = props;
+
+  const [markerClusterGroupKey, setMarkerClusterGroupKey] = useState<number>(0);
+
+  // useEffect that changes they key value when props change, in order to force a cluster group re-render
+  useEffect(() => {
+    setMarkerClusterGroupKey((prev) => prev + 1);
+  }, [props]);
+
+  // Generate markers from data
+  const markers: MarkerData[] = [];
+  const dataLength = results?.data?.length || 0;
+  if (dataLength > 0) {
+    for (let i = 0; i < dataLength || 0; i++) {
+      const latitude = results?.data?.[i][bubblePlacement.name].split(',')[0] || 0;
+      const longitude = results?.data?.[i][bubblePlacement.name].split(',')[1] || 0;
+      const count = results?.data?.[i][metric?.name || ''];
+      const metricExtended = metric as MetricExtended;
+      const title = metricExtended?.modelTitle || '';
+      markers.push({
+        title,
+        count,
+        lat: latitude,
+        lng: longitude,
+      });
+    }
+  }
+
+  // Handle values for post-facto imported components
+  const [mapComponents, setMapComponents] = useState<{
+    MapContainer: React.ComponentType<MapContainerProps>;
+    Marker: React.ComponentType<MarkerProps>;
+    MarkerClusterGroup: React.ComponentType<any>; // MarkerClusterGroupProps not exposed
+    TileLayer: React.ComponentType<TileLayerProps>;
+  } | null>(null);
+
+  // Get around "can't use require" issues by post-facto importing components
+  useEffect(() => {
+    const loadMapComponents = async () => {
+      const { MapContainer, Marker, TileLayer } = await import('react-leaflet');
+      const mc = await import('react-leaflet-markercluster');
+
+      setMapComponents({ MapContainer, Marker, TileLayer, MarkerClusterGroup: mc.default });
+    };
+    loadMapComponents();
+  }, []);
+
+  // Don't bother rendering if we don't have the components
+  if (!mapComponents) return <div>Loading map...</div>;
+
+  // Custom Cluster Icon (Bubble)
+  const createClusterCustomIcon = function (cluster: MarkerCluster) {
+    const bubbleSize =
+      cluster.getChildCount() < 10
+        ? 18
+        : Math.ceil(18 * (cluster.getChildCount().toString().length * 0.66));
+    const bubbleRadius = Math.floor(bubbleSize / 2);
+    return Leaflet.divIcon({
+      html: `<span style="display: flex; width: ${bubbleSize}px; height: ${bubbleSize}px; background-color:${markerClusterColor}; border-radius: ${bubbleRadius}px; color: #ffffff; align-items: center; justify-content: center;">${cluster.getChildCount()}</span>`,
+      className: 'custom-marker-cluster',
+      iconSize: Leaflet.point(33, 33, true),
+    });
+  };
+
+  // Custom Marker Icon (Small Bubble)
+  const createCustomIcon = function () {
+    return Leaflet.divIcon({
+      html: `<span style="display: flex; width: 10px; height: 10px; background-color:${markerColor}; border-radius: 5px; color: #ffffff; align-items: center; justify-content: center;">&nbsp;</span>`,
+      className: 'custom-marker',
+      iconSize: Leaflet.point(10, 10, true),
+    });
+  };
+
+  const { MapContainer, Marker, TileLayer, MarkerClusterGroup } = mapComponents;
+  // bounds={bounds}
+
+  return (
+    <Container {...props} className="overflow-y-hidden">
+      <div style={mapStyle}>
+        <MapContainer attributionControl={false} maxZoom={16} style={mapStyle} zoom={14}>
+          <TileLayer
+            url={customTileSet ? customTileSet : 'https://{s}.tile.osm.org/{z}/{x}/{y}.png'}
+            attribution=""
+          />
+          <SetBounds markers={markers} />
+          <MarkerClusterGroup
+            key={markerClusterGroupKey}
+            iconCreateFunction={createClusterCustomIcon}
+            maxClusterRadius={clusterRadius}
+            onClick={(e: any) => console.log('onClick', e)}
+            removeOutsideVisibleBounds
+            showCoverageOnHover={false}
+            spiderfyOnMaxZoom={true}
+          >
+            {markers.map((address, index) => {
+              const lat = address.lat;
+              const lng = address.lng;
+              return (
+                <Marker
+                  icon={createCustomIcon()}
+                  key={`${lat}-${index}-${lng}`}
+                  position={[lat, lng]}
+                  title=""
+                >
+                  {showTooltips && (
+                    <Tooltip>
+                      {address.title}: {address.count}
+                    </Tooltip>
+                  )}
+                </Marker>
+              );
+            })}
+          </MarkerClusterGroup>
+        </MapContainer>
+      </div>
+    </Container>
+  );
+};
