2e313239e7aaa19c8388ea4c30ac5d4e961b7c4d
kevinlee <kevinlee.xjtuse@gmail.com>
2025-01-15 15:53:39 +1100

    multi select dropdown with ids

---
+++ src/components/vanilla/controls/MultiSelectDropdownWithIds/MultiSelectDropdownWithIds.emb.ts
@@ -0,0 +1,140 @@
+import { Value, loadData } from '@embeddable.com/core';
+import { EmbeddedComponentMeta, Inputs, defineComponent } from '@embeddable.com/react';
+
+import Component, { Props } from './index';
+
+export const meta = {
+  name: 'MultiSelectDropdownWithIds',
+  label: 'Multi-Select dropdown with ids',
+  defaultWidth: 300,
+  defaultHeight: 80,
+  classNames: ['on-top'],
+  category: 'Controls: inputs & dropdowns',
+  inputs: [
+    {
+      name: 'ds',
+      type: 'dataset',
+      label: 'Dataset',
+      description: 'Dataset',
+      category: 'Dropdown values'
+    },
+    {
+      name: 'property',
+      type: 'dimension',
+      label: 'Property',
+      config: {
+        dataset: 'ds'
+      },
+      category: 'Dropdown values'
+    },
+    {
+      name: 'id',
+      type: 'dimension',
+      label: 'ID',
+      config: {
+        dataset: 'ds'
+      },
+      category: 'Dropdown ids'
+    },
+    {
+      name: 'title',
+      type: 'string',
+      label: 'Title',
+      category: 'Settings'
+    },
+    // {
+    //   name: 'defaultValue',
+    //   type: 'string',
+    //   array: true,
+    //   label: 'Default value',
+    //   category: 'Pre-configured variables'
+    // },
+    {
+      name: 'placeholder',
+      type: 'string',
+      label: 'Placeholder',
+      defaultValue: 'Select...',
+      category: 'Settings'
+    },
+    {
+      name: 'limit',
+      type: 'number',
+      label: 'Default number of options',
+      defaultValue: 100,
+      category: 'Settings'
+    }
+  ],
+  events: [
+    {
+      name: 'onChange',
+      label: 'Change',
+      properties: [
+        {
+          name: 'value',
+          type: 'string',
+          array: true
+        },
+        {
+          name: 'id',
+          type: 'string',
+          array: true
+        }
+      ]
+    }
+  ],
+  variables: [
+    {
+      name: 'teamNames',
+      type: 'string',
+      defaultValue: Value.noFilter(),
+      array: true,
+      // inputs: ['defaultValue'],
+      events: [{ name: 'onChange', property: 'value' }]
+    },
+    {
+      name: 'teamIds',
+      type: 'string',
+      defaultValue: Value.noFilter(),
+      array: true,
+      // inputs: ['defaultValue'],
+      events: [{ name: 'onChange', property: 'id' }]
+    }
+  ]
+} as const satisfies EmbeddedComponentMeta;
+
+export default defineComponent<Props, typeof meta, { search: string }>(Component, meta, {
+  props: (inputs: Inputs<typeof meta>, [embState]) => {
+    if (!inputs.ds)
+      return {
+        ...inputs,
+        options: [] as never
+      };
+
+    return {
+      ...inputs,
+      options: loadData({
+        from: inputs.ds,
+        dimensions: inputs.property ? [inputs.property, inputs.id] : [],
+        limit: inputs.limit || 1000,
+        filters:
+          embState?.search && inputs.property
+            ? [
+                {
+                  operator: 'contains',
+                  property: inputs.property,
+                  value: embState?.search
+                }
+              ]
+            : undefined
+      })
+    };
+  },
+  events: {
+    onChange: ({value, id}) => {
+      return {
+        value: value.length ? value : Value.noFilter(),
+        id: id.length ? id : Value.noFilter()
+      };
+    }
+  }
+});
