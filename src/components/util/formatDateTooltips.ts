3d22df5ac29a2203e84b357343ad93fa4a6cd7f5
Christopher Buecheler <chris@embeddable.com>
2025-01-17 15:15:53 +0100

    remove commented out code

--- src/components/util/formatDateTooltips.ts
+++ src/components/util/formatDateTooltips.ts
@@ -2,8 +2,6 @@ import { Granularity } from '@embeddable.com/core';
 import { format } from 'date-fns';
 import { Theme } from '../../defaulttheme';
 
-// import { DATE_DISPLAY_FORMATS } from '../constants';
-
 export default function formatDateTooltips(lines: any[], granularity: Granularity, theme: Theme) {
   const linesList = lines.map((line) => line?.parsed?.x?.valueOf());
   const set = [...new Set(linesList)];
