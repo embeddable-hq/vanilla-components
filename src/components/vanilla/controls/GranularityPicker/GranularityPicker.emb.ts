1d67fd11e7da9e89002eb7581cb445e038c429ff
Christopher Buecheler <chris@embeddable.com>
2025-04-10 10:47:35 +0200

    Add ability to include title in granularity picker

--- src/components/vanilla/controls/GranularityPicker/GranularityPicker.emb.ts
+++ src/components/vanilla/controls/GranularityPicker/GranularityPicker.emb.ts
@@ -12,79 +12,86 @@ export const meta = {
   category: 'Controls: inputs & dropdowns',
   inputs: [
     {
-        name: 'defaultValue',
-        type: 'granularity',
-        label: 'Default granularity',
-        category: 'Chart settings'
+      name: 'defaultValue',
+      type: 'granularity',
+      label: 'Default granularity',
+      category: 'Chart settings',
+    },
+    {
+      name: 'title',
+      type: 'string',
+      label: 'Title',
+      defaultValue: '',
+      category: 'Chart settings',
     },
     ...[
-        { value: 'second', defaultValue: false },
-        { value: 'minute', defaultValue: false },
-        { value: 'hour', defaultValue: false },
-        { value: 'day', defaultValue: true },
-        { value: 'week', defaultValue: true },
-        { value: 'month', defaultValue: true },
-        { value: 'quarter', defaultValue: true },
-        { value: 'year', defaultValue: true }
-    ].map((option: {value: string, defaultValue: boolean}) => ({
-        name: option.value,
-        type: 'boolean' as const,
-        label: `Display ${option.value}`,
-        defaultValue: option.defaultValue,
-        category: 'Granularity options'
-      }))
-    ],
+      { value: 'second', defaultValue: false },
+      { value: 'minute', defaultValue: false },
+      { value: 'hour', defaultValue: false },
+      { value: 'day', defaultValue: true },
+      { value: 'week', defaultValue: true },
+      { value: 'month', defaultValue: true },
+      { value: 'quarter', defaultValue: true },
+      { value: 'year', defaultValue: true },
+    ].map((option: { value: string; defaultValue: boolean }) => ({
+      name: option.value,
+      type: 'boolean' as const,
+      label: `Display ${option.value}`,
+      defaultValue: option.defaultValue,
+      category: 'Granularity options',
+    })),
+  ],
   events: [
     {
       name: 'onChange',
       label: 'Change Granularity',
       properties: [
         {
-            name: 'value',
-            type: 'granularity'
+          name: 'value',
+          type: 'granularity',
         },
         {
-            name: 'dateRange',
-            type: 'timeRange'
-        }
-      ]
-    }
+          name: 'dateRange',
+          type: 'timeRange',
+        },
+      ],
+    },
   ],
   variables: [
     {
-        name: 'granularity',
-        type: 'granularity',
-        defaultValue: 'day',
-        inputs: ['defaultValue'],
-        events: [{ name: 'onChange', property: 'value' }]
+      name: 'granularity',
+      type: 'granularity',
+      defaultValue: 'day',
+      inputs: ['defaultValue'],
+      events: [{ name: 'onChange', property: 'value' }],
     },
     {
-        name: 'date range',
-        type: 'timeRange',
-        defaultValue: { relativeTimeString: 'Last 30 days' },
-        events: [{ name: 'onChange', property: 'dateRange' }],
-      },
-  ]
+      name: 'date range',
+      type: 'timeRange',
+      defaultValue: { relativeTimeString: 'Last 30 days' },
+      events: [{ name: 'onChange', property: 'dateRange' }],
+    },
+  ],
 } as const satisfies EmbeddedComponentMeta;
 
 export default defineComponent(Component, meta, {
-    props: (inputs: Inputs<typeof meta>) => {
+  props: (inputs: Inputs<typeof meta>) => {
+    return {
+      ...inputs,
+    };
+  },
+  events: {
+    onChange: ({ value, dateRange }) => {
+      if (!dateRange.relativeTimeString) {
+        return {
+          value: value,
+          dateRange: Value.noFilter(),
+        };
+      }
       return {
-        ...inputs
+        value: value,
+        dateRange: dateRange,
       };
     },
-    events: {
-        onChange: ({value, dateRange}) => {
-            if (!dateRange.relativeTimeString) {
-                return {
-                    value: value,
-                    dateRange: Value.noFilter()
-                };
-            }
-            return {    
-                value: value,
-                dateRange: dateRange
-            };
-          },
-    }
-  });
+  },
+});
