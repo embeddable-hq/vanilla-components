f58eb1906be275627bea9f3858d23f817690243a
Ako Hosseini <alihk12@mail.com>
2025-07-08 12:35:44 +0200

    EM-2015: Add custom column label input to dimensionOrMeasure inputs (#257)

--- src/components/vanilla/charts/TableChart/components/TableHead.tsx
+++ src/components/vanilla/charts/TableChart/components/TableHead.tsx
@@ -9,54 +9,56 @@ type Props = {
   sortDirection?: SortDirection;
   onSortingChange?: (column: any, sortDirection: SortDirection) => void;
   minColumnWidth?: number;
-}
+};
 
 const TableHead = ({ columns, sortBy, sortDirection, onSortingChange, minColumnWidth }: Props) => {
   return (
     <thead className="border-y border-[#B8BDC6]">
       <tr>
-        {
-          columns.map((column) => {
-            const isSorted = sortBy?.name === column.name;
-            const newSortDirection = isSorted 
-              ? sortDirection === SortDirection.ASCENDING 
-                ? SortDirection.DESCENDING 
-                : SortDirection.ASCENDING 
-              : SortDirection.ASCENDING;
+        {columns.map((column) => {
+          const isSorted = sortBy?.name === column.name;
+          const newSortDirection = isSorted
+            ? sortDirection === SortDirection.ASCENDING
+              ? SortDirection.DESCENDING
+              : SortDirection.ASCENDING
+            : SortDirection.ASCENDING;
 
-            return (
-              <th
-                key={column.name}
-                className="bg-white select-none cursor-pointer p-3"
-                style={minColumnWidth
+          return (
+            <th
+              key={column.name}
+              className="bg-white select-none cursor-pointer p-3"
+              style={
+                minColumnWidth
                   ? {
-                    minWidth: `${minColumnWidth}px`,
-                    maxWidth: `${minColumnWidth * 1.2}px`
-                  }
+                      minWidth: `${minColumnWidth}px`,
+                      maxWidth: `${minColumnWidth * 1.2}px`,
+                    }
                   : {}
-                }
-                onClick={() => onSortingChange?.(column, newSortDirection)}
-              >
-                <div className="flex items-center gap-1 hover:text-black">
-                  <span className="text-[#333942] mr-1 truncate">
-                    {column.title}
-                  </span>
+              }
+              onClick={() => onSortingChange?.(column, newSortDirection)}
+            >
+              <div className="flex items-center gap-1 hover:text-black">
+                <span className="text-[#333942] mr-1 truncate">
+                  {/* Use the label from inputs if available, otherwise use the title */}
+                  {column?.inputs?.customColumnLabel ?? column.title}
+                </span>
 
-                  {
-                    isSorted ? (
-                      <span className="w-3">
-                        {sortDirection === SortDirection.ASCENDING ? <SortUp fill="currentcolor" /> : <SortDown fill="currentcolor" />}
-                      </span>
-                    ) : null
-                  }
-                </div>
-              </th>
-            );
-          })
-        }
+                {isSorted ? (
+                  <span className="w-3">
+                    {sortDirection === SortDirection.ASCENDING ? (
+                      <SortUp fill="currentcolor" />
+                    ) : (
+                      <SortDown fill="currentcolor" />
+                    )}
+                  </span>
+                ) : null}
+              </div>
+            </th>
+          );
+        })}
       </tr>
     </thead>
   );
-}
+};
 
 export default TableHead;
