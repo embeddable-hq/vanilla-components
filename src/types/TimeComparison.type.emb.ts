168d48ca0f69ee2e2f3ce37101a2e66eef75dd28
Christopher Buecheler <cwbuecheler@users.noreply.github.com>
2025-07-08 17:18:02 +0200

    CS-68 - Add 'previous week' selector to granularity in time comparison filter (#259)

--- src/types/TimeComparison.type.emb.ts
+++ src/types/TimeComparison.type.emb.ts
@@ -2,11 +2,12 @@ import { defineOption, defineType } from '@embeddable.com/core';
 
 const TimeComparisonType = defineType('timeComparison', {
   label: 'Time Comparison',
-  optionLabel: (value) => value
+  optionLabel: (value) => value,
 });
 
 defineOption(TimeComparisonType, 'No comparison');
 defineOption(TimeComparisonType, 'Previous period');
+defineOption(TimeComparisonType, 'Previous week');
 defineOption(TimeComparisonType, 'Previous month');
 defineOption(TimeComparisonType, 'Previous quarter');
 defineOption(TimeComparisonType, 'Previous year');
