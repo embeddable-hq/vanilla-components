a6723422adf12f0b1b536194f96087d3a9199c48
syed218 <syed.embeddable.de>
2025-07-21 19:32:16 +0200

    handle labels for both time dimension and non-time dimension

--- src/components/vanilla/charts/BarChart/components/BarChart.tsx
+++ src/components/vanilla/charts/BarChart/components/BarChart.tsx
@@ -79,17 +79,15 @@ export default function BarChart({ ...props }: Props) {
 function chartData(props: Props): ChartData<'bar' | 'line'> {
   const { results, xAxis, metrics, lineMetrics, showSecondYAxis } = props;
   const granularity = xAxis?.inputs?.granularity;
+  const isTimeDimension = xAxis?.nativeType === 'time' && granularity;
 
-  let dateFormat: string = 'yyyy-mm-dd';
-  if (xAxis.nativeType === 'time' && granularity) {
-    dateFormat = DATE_DISPLAY_FORMATS[granularity];
-  }
+  const dateFormat: string = isTimeDimension ? DATE_DISPLAY_FORMATS[granularity] : 'yyyy-mm-dd';
 
   const labels = [
     ...new Set(
       results?.data?.map((d: { [p: string]: string }) => {
         const value = d[xAxis?.name];
-        return formatValue(value === null ? '' : value, {
+        return formatValue(value ?? '', {
           meta: xAxis?.meta,
           dateFormat: dateFormat,
         });
