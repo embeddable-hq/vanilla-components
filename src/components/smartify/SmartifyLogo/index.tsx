d39e2e263ad8220006b5713a7fc83de42a4d2d7c
Nico <nickolas.lago@gmail.com>
2026-02-20 11:52:28 +0000

    games-reporting

--- src/components/smartify/SmartifyLogo/index.tsx
+++ src/components/smartify/SmartifyLogo/index.tsx
@@ -10,7 +10,7 @@ export default (props: Props) => {
   const { logoUrl } = props;
 
   return (
-    <div className="h-full flex justify-center items-center" style={{display: 'flex', maxWidth: '500px'}}>
+    <div className="h-full flex justify-center items-center" style={{display: 'flex'}}>
       <img src={logoUrl} alt="Smartify Logo" />
     </div>
   );
